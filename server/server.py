c58ac96222dd0e4542e41c938f870ecf9cb08b8f
oznob5 <geadamia@gmail.com>
2022-04-08 11:35:16 +0300

    add the server

---
+++ server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+import util
+
+app = Flask(__name__)
+
+@app.route('/classify_image', methods = ['GET', 'POST'])
+def classify_image():
+    pass
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
